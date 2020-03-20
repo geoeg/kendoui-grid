a22f10233026f5cacf77c5fd73334615b208ee31
Georgi Kolev <georgi.lyubomirov.kolev@gmail.com>
2020-03-20 18:56:40 +0200

    add pageable functionality to the table

--- src/components/MyGrid.vue
+++ src/components/MyGrid.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="my-grid">
-    <div id="selectedItem">
-      Selected Item: 
-      ID: {{ selectedItem && selectedItem.ProductID }}, 
-      Name: {{ selectedItem && selectedItem.ProductName }}, 
-      Unit Price: {{ selectedItem && selectedItem.UnitPrice }}
+    <div id="selected-ttem">
+      Selected Item: ID: {{ selectedItem && selectedItem.ProductID }}, Name:
+      {{ selectedItem && selectedItem.ProductName }}, Unit Price:
+      {{ selectedItem && selectedItem.UnitPrice }}
     </div>
     <grid
-      :style="{ height: '500px' }"
-      :data-items="items"
+      :style="{ height: 'auto' }"
+      :data-items="pageItems"
       :columns="columns"
       :selected-field="selectedField"
       @rowclick="onRowClick"
-    >
-    </grid>
+      :pageable="true"
+      :skip="skip"
+      :take="take"
+      :total="total"
+      @pagechange="pageChangeHandler"
+    ></grid>
   </div>
 </template>
 
@@ -28,13 +31,15 @@ export default {
   data: function() {
     return {
       columns: [
-        { field: "ProductID" },
+        { field: "ProductID", title: "Product ID" },
         { field: "ProductName", title: "Product Name" },
         { field: "UnitPrice", title: "Unit Price" }
       ],
       items: [],
       selectedField: "selected",
-      selectedID: 1
+      selectedID: 1,
+      skip: 0,
+      take: 10
     };
   },
   methods: {
@@ -52,7 +57,6 @@ export default {
         "Milk"
       ];
       const unitPrices = [12.5, 10.1, 5.3, 7, 22.53, 16.22, 20, 50, 100, 120];
-
       return Array(count)
         .fill({})
         .map((_, idx) => ({
@@ -64,6 +68,10 @@ export default {
     },
     onRowClick(event) {
       this.selectedID = event.dataItem.ProductID;
+    },
+    pageChangeHandler: function(event) {
+      this.skip = event.page.skip;
+      this.take = event.page.take;
     }
   },
   mounted() {
@@ -72,9 +80,24 @@ export default {
   computed: {
     selectedItem() {
       return this.items.find(item => item.ProductID == this.selectedID);
+    },
+    total() {
+      return this.items ? this.items.length : 0;
+    },
+    pageItems: {
+      get: function() {
+        return this.items.slice(this.skip, this.take + this.skip);
+      }
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.restore-columns {
+  margin: 0 0 20px;
+  padding: 20px;
+  background-color: rgba(0, 0, 0, 0.03);
+  border: 1px solid rgba(0, 0, 0, 0.08);
+}
+</style>
